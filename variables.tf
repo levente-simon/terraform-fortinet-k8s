c32e382b9352635a0399f19fe23e2b0a482dfd02
levente-simon <ls@leventesimon.com>
2022-02-18 20:00:08 +0100

    added token auth

--- variables.tf
+++ variables.tf
@@ -1,13 +1,38 @@
 variable "forti_hostname"             { type = string   }
 variable "forti_token"                { type = string   }
-variable "k8s_host"                   { type = string   }
-variable "k8s_client_certificate"     { type = string   }
-variable "k8s_client_key"             { type = string   }
-variable "k8s_cluster_ca_certificate" { type = string   }
 variable "cluster_name"               { type = string   }
 variable "cluster_hosts"              { type = map(any) }
 variable "lb_extip"                   { type = string   }
 variable "lb_dstintf"                 { type = string   }
+
+variable "k8s_host" {
+  type      = string
+  sensitive = true
+}
+
+variable "k8s_client_certificate" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "k8s_client_key" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "k8s_cluster_ca_certificate" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
+
+variable "k8s_cluster_client_token" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
 
 variable "lb_srcintf" {
   type    = string
